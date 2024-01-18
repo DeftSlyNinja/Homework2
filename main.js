3f9e624ba122552a5736199bde8567364b9d9736
DeftSlyNinja <christopher_masson@hotmail.com>
2024-01-18 14:37:42 -0800

    Fixed .map()

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
 // Fruit array that will hold the user entries
+
 let fruitData = [];
 
 document.addEventListener("DOMContentLoaded", function (event) 
 {
     //Loop that collects 3 fruit entries and assigns it to the array
+
     for (let i = 0; i < 3; i++) {
         fruitData[i] = prompt("Please enter fruit #" + (i+1))
         console.log("Fruit " + (i+1) + " = " + fruitData[i])
     }
 
     //Function that builds the list in HTML
+
     function generateList() {
         let ul = document.getElementById("fruitUL")
         for (let i = 0; i < fruitData.length; i++) {
             let li = document.createElement("li");
             ul.appendChild(li);
             li.innerHTML = fruitData[i];
+
             //Following line displays the hidden button
+
             swapBtn.style.display = "block";
         }
     }
-    //Changed array.map() to fruitData.map().
-    const swappedfruitData= fruitData.map(newArray)
-    function newArray(words2){
-        return words2;
-    }
+    //fruitData[] swapped and remapped to swappedFruitData[].
+
+    let swappedFruitData = fruitData.map(function(word) {
+        word = word.charAt(word.length - 1) + word.substring(1, word.length - 1) + word.charAt(0);
+        return word;
+    })
+
     //Event listener for the button that performs the section swap
+
     document.getElementById("swapBtn").addEventListener("click", swapSection);
+
     //Sets 1st section to hidden and displays the 2nd section
+
     function swapSection() {
-        part1.style.display = "none"
-        let ul = document.getElementById("swappedfruitUL")
-        
-        for (let i = 0; i < swappedfruitData.length; i++) {
-            swappedfruitData[i]= swappedfruitData[i].charAt(swappedfruitData[i].length-1)+
-            swappedfruitData[i].substring(1,swappedfruitData[i].length-1)+
-            swappedfruitData[i].charAt(0);
+        let ul = document.getElementById("swappedfruitUL");
+        for (let i = 0; i < swappedFruitData.length; i++) {
             let li = document.createElement("li");
             ul.appendChild(li);
-            li.innerHTML = swappedfruitData[i];
+            li.innerHTML = swappedFruitData[i];
         }
+        part1.style.display = "none"
         part2.style.display = "block"
     }
 
